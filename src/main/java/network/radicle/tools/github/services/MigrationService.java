e81ed0031ca8d308231df1838537525eb6defcc1
tdakanalis <tdakanalis@cytech.gr>
2023-05-10 09:24:53 +0300

    Updated the MigrationService in order to store map and store the GitHub issues and to the target radicle project.

--- src/main/java/network/radicle/tools/github/services/MigrationService.java
+++ src/main/java/network/radicle/tools/github/services/MigrationService.java
@@ -2,42 +2,91 @@ package network.radicle.tools.github.services;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.InternalServerErrorException;
 import network.radicle.tools.github.Config;
 import network.radicle.tools.github.clients.IGitHubClient;
+import network.radicle.tools.github.clients.IRadicleClient;
+import network.radicle.tools.github.core.github.Issue;
+import network.radicle.tools.github.core.radicle.actions.CommentAction;
+import network.radicle.tools.github.core.radicle.actions.LifecycleAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.List;
 
 @ApplicationScoped
 public class MigrationService {
     private static final Logger logger = LoggerFactory.getLogger(MigrationService.class);
 
     @Inject IGitHubClient github;
+    @Inject IRadicleClient radicle;
     @Inject Config config;
 
     public boolean migrateIssues() {
         var page = 1;
-        var hasMorePages = true;
+        var hasMoreIssues = true;
         var total = 0;
 
+        var partiallyOrNonMigratedIssues = new HashSet<Long>();
         try {
-            while (hasMorePages) {
-                logger.info("Migrating page {}", page);
+            var session = radicle.createSession();
+            logger.info("Created radicle session: {}", session.id);
+            while (hasMoreIssues) {
+                List<Issue> issues = List.of();
+                try {
+                    issues = github.getIssues(page);
+                    var batchSize = issues.size();
+                    logger.debug("Migrating page: {} with size: {}", page, batchSize);
+                    for (var issue : issues) {
+                        //ignore pull requests
+                        if (issue.pullRequest != null) {
+                            continue;
+                        }
+                        total++;
 
-                var issues = github.getIssues(page);
-                var batchSize = issues.size();
-                total += batchSize;
+                        var radIssue = issue.toRadicle();
+                        try {
+                            var id = radicle.createIssue(session, radIssue);
+                            if (id != null) {
+                                // update issue's state
+                                radicle.updateIssue(session, id, new LifecycleAction(radIssue.state));
 
-                logger.info("Fetched page {} with {} issues. Total fetched {}", page, batchSize, total);
-
-                // todo: migrate to radicle here
-
-                hasMorePages = config.getPageSize() == issues.size();
-                page++;
+                                // process issue's commends
+                                var commentsPage = 1;
+                                var hasMoreComments = true;
+                                while (hasMoreComments) {
+                                    var comments = github.getComments(issue.number, commentsPage);
+                                    for (var comment : comments) {
+                                        radicle.updateIssue(session, id, new CommentAction(comment.body));
+                                    }
+                                    hasMoreComments = config.getGithub().pageSize() == comments.size();
+                                    commentsPage++;
+                                }
+                            }
+                        } catch (Exception ex) {
+                            partiallyOrNonMigratedIssues.add(issue.number);
+                            logger.warn("Failed to migrate issue: {}, error: {}", issue.number, ex.getMessage());
+                        }
+                    }
+                    logger.info("Total processed until now: {}", total);
+                    hasMoreIssues = config.getGithub().pageSize() == issues.size();
+                    page++;
+                } catch (InternalServerErrorException | BadRequestException ex) {
+                    var ids = issues.stream().map(i -> i.number).toList();
+                    partiallyOrNonMigratedIssues.addAll(ids);
+                    logger.warn("Failed to migrate issues: {}, error: {}", ids, ex.getMessage());
+                    page++;
+                }
             }
-            logger.info("Migrated {} issues in total", total);
+            logger.info("Total processed issues: {}", total);
+            if (partiallyOrNonMigratedIssues.size() > 0) {
+                logger.warn("Partially or non migrated issues: {}", partiallyOrNonMigratedIssues);
+            }
             return true;
         } catch (Exception ex) {
-            logger.error("Migration failed with error message: {}", ex.getMessage());
+            logger.error("Migration failed: {}", ex.getMessage());
             return false;
         }
     }
