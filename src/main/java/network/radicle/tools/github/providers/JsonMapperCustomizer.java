032739a1166174e162d99219d10f82041882c7d8
tdakanalis <tdakanalis@cytech.gr>
2023-05-10 12:08:28 +0300

    Removed non required jackson data-type binding dependencies

--- src/main/java/network/radicle/tools/github/providers/JsonMapperCustomizer.java
+++ src/main/java/network/radicle/tools/github/providers/JsonMapperCustomizer.java
@@ -3,7 +3,6 @@ package network.radicle.tools.github.providers;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.inject.Singleton;
 
@@ -14,7 +13,6 @@ public class JsonMapperCustomizer implements ObjectMapperCustomizer {
     public void customize(ObjectMapper objectMapper) {
         objectMapper.findAndRegisterModules()
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .registerModule(new JavaTimeModule());
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
