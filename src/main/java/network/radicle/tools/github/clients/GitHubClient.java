b7e7f08cb02972e3fa1d92ae2be6aa1fc0e17942
tdakanalis <tdakanalis@cytech.gr>
2023-05-10 09:22:31 +0300

    Implemented the getComments method in the GitHubClient

--- src/main/java/network/radicle/tools/github/clients/GitHubClient.java
+++ src/main/java/network/radicle/tools/github/clients/GitHubClient.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.core.HttpHeaders;
-import jakarta.ws.rs.core.Response;
 import network.radicle.tools.github.Config;
-import network.radicle.tools.github.core.GitHubIssue;
+import network.radicle.tools.github.core.github.Comment;
+import network.radicle.tools.github.core.github.Issue;
+import network.radicle.tools.github.handlers.ResponseHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,32 +24,48 @@ public class GitHubClient implements IGitHubClient {
     @Inject ObjectMapper mapper;
     @Inject Config config;
 
-    public List<GitHubIssue> getIssues(int page) throws Exception {
-        var url = String.join("/", List.of(Strings.nullToEmpty(config.getUrl()), "repos",
-                Strings.nullToEmpty(config.getOwner()), Strings.nullToEmpty(config.getRepo()), "issues"));
+    public List<Issue> getIssues(int page) throws Exception {
+        var url = String.join("/", List.of(
+                Strings.nullToEmpty(config.getGithub().url()), "repos",
+                Strings.nullToEmpty(config.getGithub().owner()),
+                Strings.nullToEmpty(config.getGithub().repo()), "issues"));
 
-        logger.info("Fetching issues from {}", url);
-
+        logger.debug("Fetching GitHub issues: {}", url);
         try (var resp = client.target(url)
                 .queryParam("state", "all")
-                .queryParam("per_page", config.getPageSize())
+                .queryParam("per_page", config.getGithub().pageSize())
                 .queryParam("page", page)
                 .request()
                 .header(HttpHeaders.ACCEPT, "application/vnd.github+json")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + Strings.nullToEmpty(config.getToken()))
-                .header("X-GitHub-Api-Version", config.getVersion())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + Strings.nullToEmpty(config.getGithub().token()))
+                .header("X-GitHub-Api-Version", config.getGithub().version())
                 .get()) {
 
-            var status = resp.getStatus();
-            var json = Strings.nullToEmpty(resp.readEntity(String.class));
-
-            if (Response.Status.Family.familyOf(status) != Response.Status.Family.SUCCESSFUL) {
-                logger.error("Got status: {}, payload: {}", status, json);
-                throw new BadRequestException(json);
-            }
-
+            var json = ResponseHandler.handleResponse(resp);
             return mapper.readValue(json, new TypeReference<>() { });
+        }
+    }
 
+    @Override
+    public List<Comment> getComments(long issueId, int page) throws Exception {
+        var url = String.join("/", List.of(
+                Strings.nullToEmpty(config.getGithub().url()), "repos",
+                Strings.nullToEmpty(config.getGithub().owner()),
+                Strings.nullToEmpty(config.getGithub().repo()), "issues",
+                Long.toString(issueId), "comments"));
+
+        logger.debug("Fetching GitHub comments for issue {}: {}", issueId, url);
+        try (var resp = client.target(url)
+                .queryParam("per_page", config.getGithub().pageSize())
+                .queryParam("page", page)
+                .request()
+                .header(HttpHeaders.ACCEPT, "application/vnd.github+json")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + Strings.nullToEmpty(config.getGithub().token()))
+                .header("X-GitHub-Api-Version", config.getGithub().version())
+                .get()) {
+
+            var json = ResponseHandler.handleResponse(resp);
+            return mapper.readValue(json, new TypeReference<>() { });
         }
     }
 }
