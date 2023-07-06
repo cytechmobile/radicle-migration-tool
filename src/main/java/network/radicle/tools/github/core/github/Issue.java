fa7fe62c221eeba01428a6529cd2ba2e59e9e915
tdakanalis <tdakanalis@cytech.gr>
2023-07-06 18:16:29 +0300

    Migrate GitHub Milestones
    Migrated the GitHub Milestones as part of the GitHub metadata of the Radicle Issue. To increase visibility, the title of the milestone was also migrated as a tag in the Radicle Issue.

--- src/main/java/network/radicle/tools/github/core/github/Issue.java
+++ src/main/java/network/radicle/tools/github/core/github/Issue.java
@@ -5,14 +5,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Strings;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import network.radicle.tools.github.core.radicle.State;
+import network.radicle.tools.github.utils.Markdown;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static network.radicle.tools.github.core.github.Timeline.DTF;
+import static network.radicle.tools.github.utils.Markdown.escape;
+import static network.radicle.tools.github.utils.Markdown.link;
 
 @RegisterForReflection
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Issue {
+    public static final String METADATA_TITLE = "GitHub Metadata";
     public static final String STATE_OPEN = "open";
     public static final String STATE_CLOSED = "closed";
     public static final String STATE_COMPLETED = "completed";
@@ -78,10 +85,19 @@ public class Issue {
         var issue = new network.radicle.tools.github.core.radicle.Issue();
 
         issue.title = this.title;
-        issue.description = getMeta() + "\n\n" +  Strings.nullToEmpty(this.body);
+        var meta = getMeta();
+        issue.description = Strings.isNullOrEmpty(meta) ?
+                Strings.nullToEmpty(this.body) :
+                meta + "<br/>" +  Strings.nullToEmpty(this.body);
         issue.tags = this.labels != null ?
                 this.labels.stream().map(l -> l.name).collect(Collectors.toList()) :
                 List.of();
+
+        if (this.milestone != null) {
+            var tags = new ArrayList<>(issue.tags);
+            tags.add(this.milestone.title);
+            issue.tags = tags;
+        }
 
         var reason = "";
         if (STATE_OPEN.equalsIgnoreCase(this.state)) {
@@ -96,14 +112,41 @@ public class Issue {
     }
 
     public String getMeta() {
-        var meta = "> github issue: #" + this.number + " created on " +
-                Timeline.DTF.format(this.createdAt).replace(":", "\\:") + " by " + this.user.login;
+        var metadata = new Markdown().openDropDown(METADATA_TITLE);
 
-        if (this.assignees != null && !this.assignees.isEmpty()) {
-            meta += " and assigned to " + this.assignees.stream().map(a -> a.login)
-                    .collect(Collectors.joining(", "));
+        var headers = new ArrayList<>();
+        headers.add("Number");
+        headers.add("Created On");
+        headers.add("Created By");
+
+        var hasAssignees = this.assignees != null && !this.assignees.isEmpty();
+        if (hasAssignees) {
+            headers.add("Assignees");
         }
-        return meta;
+        var hasMilestone = this.milestone != null;
+        if (hasMilestone) {
+            headers.add("Milestone");
+            headers.add("Due By");
+        }
+        metadata.openTable(headers.toArray());
+
+        var rows = new ArrayList<>();
+        rows.add(link("#" + this.number, this.htmlUrl));
+        rows.add(escape(DTF.format(this.createdAt)));
+        rows.add(link(this.user.login, this.user.htmlUrl));
+        if (hasAssignees) {
+            rows.add(this.assignees.stream()
+                    .map(a -> link(a.login, a.htmlUrl))
+                    .collect(Collectors.joining(", ")));
+        }
+        if (hasMilestone) {
+            rows.add(link(this.milestone.title, this.milestone.htmlUrl));
+            rows.add(escape(DTF.format(Instant.parse(this.milestone.dueOn))));
+        }
+        metadata.addTableRow(rows.toArray());
+        metadata.closeTable().closeDropDown();
+
+        return metadata.build();
     }
 
     @Override
