00194aa8c858c2d0e82aa5d567d7fdac5795be5b
tdakanalis <tdakanalis@cytech.gr>
2023-10-20 20:11:22 +0300

    test(backend): Mime Type Detection
    Adds a test to check the mimetype detection using the Tike() facade.
    
    Signed-off-by: tdakanalis <tdakanalis@cytech.gr>

--- src/test/java/network/radicle/tools/github/services/FileStorageServiceTest.java
+++ src/test/java/network/radicle/tools/github/services/FileStorageServiceTest.java
@@ -1,10 +1,12 @@
 package network.radicle.tools.github.services;
 
+import network.radicle.tools.github.core.radicle.actions.EmbedTest;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import java.util.Base64;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileStorageServiceTest {
 
@@ -21,5 +23,18 @@ public class FileStorageServiceTest {
 
         var property = service.getProperty(name);
         assertThat(property).isEqualTo(value);
+    }
+
+    @Test
+    void testMimeTypeDetection() {
+        var embed = EmbedTest.generateEmbed();
+        assertThat(embed).isNotNull();
+
+        var parts = embed.content.split(",");
+        var expectedHeader = parts[0] + ",";
+
+        var fileService = new FilesService();
+        var actualHeader = fileService.getBase64Prefix(Base64.getDecoder().decode(parts[1]));
+        assertThat(actualHeader).isEqualTo(expectedHeader);
     }
 }
