6451ce5f1a2eef4461e360a9bcfd58ef45eebdb7
tdakanalis <tdakanalis@cytech.gr>
2023-10-23 12:10:05 +0300

    feat(backend): Replace Ssh Agent with Keys Handling
    Replaces session signing logic to eliminate reliance on the SSH agent. Now, the signing process is handled internally, utilizing the provided private key.

--- src/main/java/network/radicle/tools/github/services/CliService.java
+++ src/main/java/network/radicle/tools/github/services/CliService.java
@@ -6,8 +6,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import network.radicle.tools.github.Config;
 import network.radicle.tools.github.core.radicle.Session;
-import network.radicle.tools.github.utils.Multibase;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +13,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class CliService {
@@ -29,12 +21,6 @@ public class CliService {
 
     @Inject Config config;
     @Inject ObjectMapper mapper;
-
-    @ConfigProperty(name = "rad.passphrase")
-    Optional<String> passphrase;
-
-    @ConfigProperty(name = "rad.home")
-    Optional<String> radHome;
 
     public Session createSession() {
         try {
@@ -60,57 +46,15 @@ public class CliService {
         }
     }
 
-    // [PROTOCOL.sshsig]: https://cvsweb.openbsd.org/src/usr.bin/ssh/PROTOCOL.sshsig?annotate=HEAD
-    public String sign(Session session) {
-        try {
-            if (radHome.isEmpty()) {
-                logger.debug("RAD_HOME is missing");
-                return null;
-            }
-
-            var dataToSign = session.id + ":" + session.publicKey;
-            var sourceTempFile = Files.createTempFile(null, null);
-            Files.writeString(sourceTempFile, dataToSign);
-
-            var dataToSignFromFile = Files.readAllBytes(sourceTempFile);
-            logger.info("SSH-KEYGEN DATA TO SIGN: {}", dataToSignFromFile);
-
-            var sigTempFile = Files.createTempFile(null, null);
-
-            var command = "cat " + sourceTempFile + " | " +
-            "ssh-keygen -Y sign -n file -f " + radHome.get() + "/keys/radicle > " + sigTempFile;
-
-            logger.debug("{}", command);
-
-            execCommand(command, passphrase.orElse(null));
-
-            var lines = Files.readAllLines(sigTempFile);
-            var signed = "";
-            if (lines.size() > 2) {
-                signed = lines.stream().skip(1).limit(lines.size() - 2).collect(Collectors.joining());
-            }
-
-            //the 8 is for the integers that keep the header's and signature's lengths
-            var base64Decoded = Base64.getDecoder().decode(signed);
-            var signature = Arrays.copyOfRange(base64Decoded, base64Decoded.length - 64, base64Decoded.length);
-
-            var s = Multibase.encode(Multibase.Base.Base58BTC, signature);
-            var log = Map.of("sessionId", session.id, "publicKey", session.publicKey, "signature", s);
-            logger.debug("{}, {}", mapper.writeValueAsString(log), base64Decoded);
-
-            return s;
-        } catch (Exception ex) {
-            logger.error("Failed to sign the session using the ssh-keygen -Y", ex);
-            return null;
-        }
-    }
-
     public String execCommand(String command, String userInput) {
         var output = new StringBuilder();
 
         try {
             var builder = new ProcessBuilder("/bin/sh", "-c", command);
-            passphrase.ifPresent(s -> builder.environment().put("RAD_PASSPHRASE", s));
+            var passphrase = config.getRadicle().passphrase();
+            if (!Strings.isNullOrEmpty(passphrase)) {
+                builder.environment().put("RAD_PASSPHRASE", passphrase);
+            }
 
             var process = builder.start();
 
